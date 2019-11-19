1a977672bcb6efe7ba76b551f071045cd89697d0
NH333 <842414969@qq.com>
2019-11-18 23:59:26 -0500

    ch10.3

---
+++ caq/cppPrimer/ch10/10_14.cpp
@@ -0,0 +1,6 @@
+//10_4
+auto sum = [](int a, int b) {return a + b; };
+
+//10_5
+int i = 3;
+auto sum2 = [i](int b) {return i + b; };
---
+++ caq/cppPrimer/ch10/10_22.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using std::vector;
+using std::string;
+using std::cout;
+using std::endl;
+
+int CountWords(const vector<string> m_vec, int sz) {
+	int count = 0;
+	for (const auto& i : m_vec) {
+		if (i.size()<=sz)
+		{
+			++count;
+		}
+	}
+	return count;
+}
+
+int main() {
+	vector<string> m_vector = { "chenanqi","anqi","aa","1234567","12" };
+	int sum = CountWords(m_vector, 6);
+	cout << sum << endl;
+	system("pause");
+}
