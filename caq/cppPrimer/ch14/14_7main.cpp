036c5fd1de95145bc3002109ec126de41b495c4c
NH333 <842414969@qq.com>
2020-01-26 16:26:31 -0500

    out is in ostream not in ofstream!!!f---uck---
    STL MY String

---
+++ caq/cppPrimer/ch14/14_7main.cpp
@@ -0,0 +1,20 @@
+#include"13_44.h"
+#include <vector>
+#include <iostream>
+
+int main() {
+	
+	String a("abc");
+	//String c = a;
+	//String b = c + a;
+	/*
+	std::vector<String> m_vec = {};
+	m_vec.push_back("chen");
+	m_vec.push_back("anqi");
+	m_vec.push_back("chen anqi");*/
+	
+	std::cout << a << std::endl;
+
+	system("pause");
+
+}
